75dd55897aeb3d0c954041b1fa19cf703dc82990
arvind <akmanutd01@yahoo.in>
2025-03-13 01:15:20 -0500

    V1.2.0

--- scraper.js
+++ scraper.js
@@ -13,31 +13,64 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const fs_1 = __importDefault(require("fs"));
+const filePath = "data.json";
+const initializeFile = () => {
+    if (!fs_1.default.existsSync(filePath)) {
+        fs_1.default.writeFileSync(filePath, JSON.stringify({}, null, 2));
+        console.log("Created data.json file with an empty object");
+    }
+};
+initializeFile();
 const targetText = "Apple iPhone 15 Pro Max, 256GB, Black Titanium - Unlocked (Renewed Premium)";
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    var userAgent = require('user-agents');
+    var userAgent = require("user-agents");
     const browser = yield puppeteer_1.default.launch({
         headless: false,
-        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
     });
     const page = yield browser.newPage();
     yield page.setUserAgent(userAgent.random().toString());
-    yield page.goto('https://amazon.com/', { waitUntil: 'load' });
+    yield page.goto("https://amazon.com/", { waitUntil: "load" });
     // await page.setViewport({width: 1080, height: 1024});
     yield page.locator('input[type="text"]').fill(targetText);
-    page.keyboard.press('Enter');
+    page.keyboard.press("Enter");
     yield page.waitForNavigation();
     const listing = yield page.evaluate((targetText) => {
-        const listingURLS = document.querySelectorAll('a');
-        return Array.from(listingURLS).map((item => {
-            const h2 = item.querySelector('h2');
-            if ((h2 === null || h2 === void 0 ? void 0 : h2.getAttribute('aria-label')) === targetText) {
+        const listingURLS = document.querySelectorAll("a");
+        return Array.from(listingURLS)
+            .map((item) => {
+            const h2 = item.querySelector("h2");
+            if ((h2 === null || h2 === void 0 ? void 0 : h2.getAttribute("aria-label")) === targetText) {
                 return item.href;
             }
-        })).filter(Boolean);
+        })
+            .filter(Boolean);
     }, targetText);
-    yield page.goto('' + listing[0], { waitUntil: 'load' });
-    const listingImage = yield page.$eval('img#main-image', el => el.getAttribute('src'), { timeout: 5000 });
-    const listingPrice = yield page.$eval('span.apexPriceToPay > span', el => el.innerHTML, { timeout: 5000 });
+    yield page.goto("" + listing[0], { waitUntil: "load" });
+    let listingImage;
+    let listingPrice;
+    try {
+        listingImage = yield page.$eval("img#main-image", (el) => el.getAttribute("src"), { timeout: 5000 });
+    }
+    catch (error) {
+        console.error("Error extracting the image:", error);
+    }
+    try {
+        listingPrice = yield page.$eval("span.apexPriceToPay > span", (el) => el.innerHTML, { timeout: 5000 });
+    }
+    catch (error) {
+        console.error("Error extracting the price:", error);
+    }
+    let data = {
+        image: listingImage,
+        price: listingPrice,
+    };
+    let extractedTime = new Date().toJSON();
+    const existingData = fs_1.default.readFileSync(filePath, "utf-8");
+    const parsedData = JSON.parse(existingData);
+    parsedData[extractedTime] = data;
+    fs_1.default.writeFileSync(filePath, JSON.stringify(parsedData, null, 2));
+    console.log("Data appended to", filePath);
     browser.close();
 }))();
--- scraper.ts
+++ scraper.ts
@@ -1,39 +1,85 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
+import fs from "fs";
 
-const targetText: string = "Apple iPhone 15 Pro Max, 256GB, Black Titanium - Unlocked (Renewed Premium)";
+const filePath = "data.json";
+const initializeFile = () => {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify({}, null, 2));
+    console.log("Created data.json file with an empty object");
+  }
+};
+initializeFile();
+
+const targetText: string =
+  "Apple iPhone 15 Pro Max, 256GB, Black Titanium - Unlocked (Renewed Premium)";
 
 (async () => {
-  var userAgent = require('user-agents');
-  
+  var userAgent = require("user-agents");
+
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-})
+    executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+  });
+  
   const page = await browser.newPage();
-  await page.setUserAgent(userAgent.random().toString())
-
-  await page.goto('https://amazon.com/', { waitUntil: 'load' });
+  await page.setUserAgent(userAgent.random().toString());
+  await page.goto("https://amazon.com/", { waitUntil: "load" });
 
   // await page.setViewport({width: 1080, height: 1024});
-  
+
   await page.locator('input[type="text"]').fill(targetText);
-  page.keyboard.press('Enter');
+  page.keyboard.press("Enter");
   await page.waitForNavigation();
-  
+
   const listing = await page.evaluate((targetText) => {
-    const listingURLS = document.querySelectorAll('a');
-    return Array.from(listingURLS).map((item => {
-      const h2 = item.querySelector('h2');
-      if (h2?.getAttribute('aria-label') === targetText) {
-        return item.href;
-      }
-    })
-  ).filter(Boolean)
-}, targetText)
-await page.goto(''+listing[0], { waitUntil: 'load' });
+    const listingURLS = document.querySelectorAll("a");
+    return Array.from(listingURLS)
+      .map((item) => {
+        const h2 = item.querySelector("h2");
+        if (h2?.getAttribute("aria-label") === targetText) {
+          return item.href;
+        }
+      })
+      .filter(Boolean);
+  }, targetText);
+  
+  await page.goto("" + listing[0], { waitUntil: "load" });
 
-const listingImage = await page.$eval('img#main-image', el => el.getAttribute('src'), { timeout: 5000 });
-const listingPrice = await page.$eval('span.apexPriceToPay > span', el => el.innerHTML, { timeout: 5000 });
+  let listingImage;
+  let listingPrice;
+  
+  try {
+    listingImage = await page.$eval(
+      "img#main-image",
+      (el) => el.getAttribute("src"),
+      { timeout: 5000 }
+    );
+  } catch (error) {
+    console.error("Error extracting the image:", error);
+  }
+  
+  try {
+    listingPrice = await page.$eval(
+      "span.apexPriceToPay > span",
+      (el) => el.innerHTML,
+      { timeout: 5000 }
+    );
+  } catch (error) {
+    console.error("Error extracting the price:", error);
+  }
 
-browser.close();
+  let data = {
+    image: listingImage,
+    price: listingPrice,
+  };
+
+  let extractedTime = new Date().toJSON();
+
+  const existingData = fs.readFileSync(filePath, "utf-8");
+  const parsedData = JSON.parse(existingData);
+  parsedData[extractedTime] = data;
+  fs.writeFileSync(filePath, JSON.stringify(parsedData, null, 2));
+  console.log("Data appended to", filePath);
+
+  browser.close();
 })();
